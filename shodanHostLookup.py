287e2223c37ea05647d97bebf8dce0ea7b15ec71
MxSns <maxime.sn210@gmail.com>
2025-12-16 12:47:02 +0100

    update

--- shodanHostLookup.py
+++ shodanHostLookup.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import json, requests, argparse, os
 
 parser = argparse.ArgumentParser(prog='shodanHostLookup', description='This script returns the shodan API result for an IP, turn it to json and print it')
@@ -9,26 +11,24 @@ parser.add_argument('-o', '--output', help='The path for the output file')
 
 args = parser.parse_args()
 
-# Variables
+
 api_key = (args.key)
 
-# Lookup an IP using shodan's REST API
+# shodan REST API
 response = requests.get(f'https://api.shodan.io/shodan/host/{args.host}?key={api_key}')
 
 ipinfo_dict = json.loads(response.text)
 
-# Format the output
+
 host_info_json = json.dumps(ipinfo_dict, indent=4, sort_keys=True, ensure_ascii=False)
 
-# Create the path and dir for the output
+# path and dir for the output
 
 if args.output:     
     output_file = os.path.join(args.output)
     with open(output_file, 'w') as f:
         f.write(host_info_json)
     print(f"Results saved to {output_file}")
-
-# Print the hostname from the dictionary
 
 print ("\nDetailed response:\n")
 for key, value in ipinfo_dict.items():
